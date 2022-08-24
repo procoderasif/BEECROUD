c77b38c7d83f2283bce57d0519aa40db899081d7
Asifur Rahaman <110245664+procoderasif@users.noreply.github.com>
2022-08-25 01:09:53 +0600

    1060.positive number

---
+++ positive number.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+{
+    int a,b,c,d,e,even=0,odd=0;
+    cin>>a;
+    if(a%2==0)
+    {
+        even++;
+
+    }
+    else
+        {
+            odd++;
+        }
+
+
+
+
+
+
+
+    return 0;
+}
